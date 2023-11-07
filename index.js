163dd8aa6dd5a25c0bae3b6dfc80220f572e474b
shimu-7 <shamima19shimu@gmail.com>
2023-11-07 21:16:26 +0600

    an api is created to add and get jobs

--- index.js
+++ index.js
@@ -26,23 +26,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
+    const categoryCollection = client.db('jobDB').collection('category');
+    const jobCollection = client.db('jobDB').collection('jobs');
+    app.get('/category', async (req, res) => {
+      const cursor = categoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/jobs', async (req, res) => {
+      const newJob = req.body;
+      console.log(newJob);
+      const result = await jobCollection.insertOne(newJob);
+      res.send(result);
+  })
+
+  app.get('/jobs', async (req, res) => {
+    const cursor = jobCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-app.get('/',(req, res)=>{
-    res.send('job Seeking server is running');
+app.get('/', (req, res) => {
+  res.send('job Seeking server is running');
 })
 
-app.listen(port,()=>{
-    console.log(`server running on ${port}`);
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
 })
